4c6cbf2819283602487c41a8e7409a154932128d
Jagrati Dangayach <jagratidangayach@gmail.com>
2019-04-02 00:36:41 +0530

    Add project files.

---
+++ BusinessLogic/AddLogic.cs
@@ -0,0 +1,24 @@
+﻿using SumOfNumbers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers.BusinessLogic
+{
+    public class AddLogic : IAddNumbersLogic
+    {
+        /// <summary>
+        /// Logic to add the numbers
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        public int AddNumbers(SumModel sum)
+        {
+            int res = sum.FirstNumber + sum.SecondNumber;
+            return res;
+
+        }
+    }
+}
+
---
+++ BusinessLogic/IAddNumbersLogic.cs
@@ -0,0 +1,16 @@
+﻿using SumOfNumbers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SumOfNumbers.BusinessLogic
+{
+    /// <summary>
+    /// This interface is used to get sum of the two numbers
+    /// </summary>
+    public interface IAddNumbersLogic 
+    {
+         int AddNumbers(SumModel model);
+    }
+}
